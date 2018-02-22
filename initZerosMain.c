63b97c2252ce5648e6f91dcdfd062bdd904938a3
Ashraf-Omar <ashrafomar6@gmail.com>
2018-02-22 23:51:54 +0200

    Added setOnes.h. Edited initZerosMain.c

--- initZerosMain.c
+++ initZerosMain.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "initZeros.h"
+#include "setOnes.h"
 
 int main() {
 	printf("Enter the dimensions of your matrix. Eg. If 3D type 3 if 10D type 10\n");
@@ -23,5 +25,13 @@ int main() {
 	int sizeOfArray = getArraySize((int *)bounds,numOfDimensions);//Get the size of the array
 	int *arr = (int*)malloc(sizeOfArray*sizeof(int));//Allocate memory for an array of the given size
 	initZeros(arr,(int *)bounds,numOfDimensions);//Initialize array to zeros
+	setOnes(arr,(int *)bounds,numOfDimensions);//Set 10% of the array to one
+/*
+	for (int i=0;i<sizeOfArray;i++)//Create a one-dimensional array to represent the multidimensional one.
+	{
+		printf("%d",arr[i]);
+	}
+	printf("\n");
+*/
 	free(arr);//Free the memory allocated for the array. This is necessary in order to avoid a memory leak.
 }
